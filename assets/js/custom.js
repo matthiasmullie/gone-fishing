9f6655c265b90a2b5315c53c1d2823c40018b5bf
Matthias Mullie <git@mullie.eu>
2017-05-16 14:17:42 +0200

    Some more polish

---
+++ assets/js/custom.js
@@ -0,0 +1,117 @@
+$(document).ready(function () {
+    'use strict';
+    function loadMap() {
+        var styles = [
+            {
+                featureType: "landscape",
+                elementType: "geometry.fill",
+                stylers: [{ color: "#ffffff" }]
+            },
+            {
+                featureType: "administrative",
+                elementType: "geometry.stroke",
+                stylers: [{ color: "#bbbbbb" }]
+            },
+            {
+                featureType: "poi",
+                elementType: "all",
+                stylers: [{ visibility: "off" }]
+            },
+            {
+                featureType: "poi.park",
+                elementType: "geometry.fill",
+                stylers: [{ color: "#e6f3d6" }, { visibility: "on" }]
+            },
+            {
+                featureType: "road",
+                elementType: "all",
+                stylers: [{ saturation: -100 }, { lightness: 45 }, { visibility: "simplified" }]
+            },
+            {
+                featureType: "road.highway",
+                elementType: "all",
+                stylers: [{ visibility: "simplified" }]
+            },
+            {
+                featureType: "road.highway",
+                elementType: "geometry.fill",
+                stylers: [{ color: "#f4d2c5" }, { visibility: "simplified" }]
+            },
+            {
+                featureType: "road.highway",
+                elementType: "labels.text",
+                stylers: [{ color: "#4e4e4e" }]
+            },
+            {
+                featureType: "road.arterial",
+                elementType: "geometry.fill",
+                stylers: [{ color: "#f4f4f4" }]
+            },
+            {
+                featureType: "road.arterial",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#787878" }]
+            },
+            {
+                featureType: "water",
+                elementType: "all",
+                stylers: [{ visibility: "simplified" }, { color: "#5f94ff" }, { lightness: 26 }, { gamma: 5.86 }]
+            },
+            {
+                featureType: "transit",
+                elementType: "all",
+                stylers: [{ visibility: "off" }]
+            },
+            {
+                elementType: "labels.text.stroke",
+                stylers: [{ visibility: "off" }]
+            },
+            {
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#888888" }]
+            },
+            {
+                elementType: "labels.icon",
+                stylers: [{ visibility: "off" }]
+            }
+        ],
+        node = document.getElementById('map'),
+        coordinate = new google.maps.LatLng(node.getAttribute('data-lat'), node.getAttribute('data-lon')),
+        options = {
+            center: coordinate,
+            zoom: 5,
+            styles: styles,
+            scrollwheel: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        },
+        map = new google.maps.Map(node, options),
+        marker = new google.maps.Marker({
+            position: coordinate,
+            map: map
+        });
+    }
+
+    $('.responsive-menu').on('click', function (e) {
+        e.stopPropagation();
+        e.preventDefault();
+        $('.main-nav > ul').css({ display: 'block' });
+        return false;
+    });
+    $('body').on('click', function () {
+        $('.main-nav > ul').css({ display: 'none' });
+    });
+    $('.go-to-top').on('click', function () {
+        $('body, html').animate({ scrollTop: 0 }, 1200, 'swing');
+        return false;
+    });
+
+    $(function() {
+        $('.photo a').fluidbox();
+    });
+
+    if ($('#map').length) {
+        google.maps.event.addDomListener(window, 'load', loadMap);
+    }
+
+    $('textarea').autoExpand({ animationTime: 20 });
+});
