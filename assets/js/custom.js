c83f9ae9a32bf1f8bf0398207c635122e6a31772
JnNPLr^NoLYN6 <mullie-bot>
2017-09-06 09:54:03 +0000

    Deploy to GitHub Pages: 0b272722f8c6fbe71a795e75d3ade8fde2d060ed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,87 +3,90 @@ $(document).ready(function () {
 
     var map = {
         map: null,
-        options: {
-            scrollwheel: false,
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            styles: [
-                {
-                    featureType: "landscape",
-                    elementType: "geometry.fill",
-                    stylers: [{ color: "#ffffff" }]
-                },
-                {
-                    featureType: "administrative",
-                    elementType: "geometry.stroke",
-                    stylers: [{ color: "#bbbbbb" }]
-                },
-                {
-                    featureType: "poi",
-                    elementType: "all",
-                    stylers: [{ visibility: "off" }]
-                },
-                {
-                    featureType: "poi.park",
-                    elementType: "geometry.fill",
-                    stylers: [{ color: "#e6f3d6" }, { visibility: "on" }]
-                },
-                {
-                    featureType: "road",
-                    elementType: "all",
-                    stylers: [{ saturation: -100 }, { lightness: 45 }, { visibility: "simplified" }]
-                },
-                {
-                    featureType: "road.highway",
-                    elementType: "all",
-                    stylers: [{ visibility: "simplified" }]
-                },
-                {
-                    featureType: "road.highway",
-                    elementType: "geometry.fill",
-                    stylers: [{ color: "#f4d2c5" }, { visibility: "simplified" }]
-                },
-                {
-                    featureType: "road.highway",
-                    elementType: "labels.text",
-                    stylers: [{ color: "#4e4e4e" }]
-                },
-                {
-                    featureType: "road.arterial",
-                    elementType: "geometry.fill",
-                    stylers: [{ color: "#f4f4f4" }]
-                },
-                {
-                    featureType: "road.arterial",
-                    elementType: "labels.text.fill",
-                    stylers: [{ color: "#787878" }]
-                },
-                {
-                    featureType: "water",
-                    elementType: "all",
-                    stylers: [{ visibility: "simplified" }, { color: "#5f94ff" }, { lightness: 26 }, { gamma: 5.86 }]
-                },
-                {
-                    featureType: "transit",
-                    elementType: "all",
-                    stylers: [{ visibility: "off" }]
-                },
-                {
-                    elementType: "labels.text.stroke",
-                    stylers: [{ visibility: "off" }]
-                },
-                {
-                    elementType: "labels.text.fill",
-                    stylers: [{ color: "#888888" }]
-                },
-                {
-                    elementType: "labels.icon",
-                    stylers: [{ visibility: "off" }]
-                }
-            ]
+
+        getOptions: function () {
+            return {
+                scrollwheel: false,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                styles: [
+                    {
+                        featureType: "landscape",
+                        elementType: "geometry.fill",
+                        stylers: [{ color: "#ffffff" }]
+                    },
+                    {
+                        featureType: "administrative",
+                        elementType: "geometry.stroke",
+                        stylers: [{ color: "#bbbbbb" }]
+                    },
+                    {
+                        featureType: "poi",
+                        elementType: "all",
+                        stylers: [{ visibility: "off" }]
+                    },
+                    {
+                        featureType: "poi.park",
+                        elementType: "geometry.fill",
+                        stylers: [{ color: "#e6f3d6" }, { visibility: "on" }]
+                    },
+                    {
+                        featureType: "road",
+                        elementType: "all",
+                        stylers: [{ saturation: -100 }, { lightness: 45 }, { visibility: "simplified" }]
+                    },
+                    {
+                        featureType: "road.highway",
+                        elementType: "all",
+                        stylers: [{ visibility: "simplified" }]
+                    },
+                    {
+                        featureType: "road.highway",
+                        elementType: "geometry.fill",
+                        stylers: [{ color: "#f4d2c5" }, { visibility: "simplified" }]
+                    },
+                    {
+                        featureType: "road.highway",
+                        elementType: "labels.text",
+                        stylers: [{ color: "#4e4e4e" }]
+                    },
+                    {
+                        featureType: "road.arterial",
+                        elementType: "geometry.fill",
+                        stylers: [{ color: "#f4f4f4" }]
+                    },
+                    {
+                        featureType: "road.arterial",
+                        elementType: "labels.text.fill",
+                        stylers: [{ color: "#787878" }]
+                    },
+                    {
+                        featureType: "water",
+                        elementType: "all",
+                        stylers: [{ visibility: "simplified" }, { color: "#5f94ff" }, { lightness: 26 }, { gamma: 5.86 }]
+                    },
+                    {
+                        featureType: "transit",
+                        elementType: "all",
+                        stylers: [{ visibility: "off" }]
+                    },
+                    {
+                        elementType: "labels.text.stroke",
+                        stylers: [{ visibility: "off" }]
+                    },
+                    {
+                        elementType: "labels.text.fill",
+                        stylers: [{ color: "#888888" }]
+                    },
+                    {
+                        elementType: "labels.icon",
+                        stylers: [{ visibility: "off" }]
+                    }
+                ]
+            }
         },
 
         load: function (node, config) {
-            this.map = new google.maps.Map(node, this.options);
+            this.map = new google.maps.Map(node, this.getOptions());
 
             this.setCenter(config.center);
             this.setZoom(config.zoom);
@@ -136,7 +139,6 @@ $(document).ready(function () {
                 coordinate,
                 start,
                 stop,
-                legs,
                 i;
 
             if (!coordinates || coordinates.length === 0) {
